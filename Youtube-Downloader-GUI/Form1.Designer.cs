0fd04c70cff8dfb3fc10b5d0e33305fa18b3e000
EricNohara <ernohara@bu.edu>
2025-10-26 01:49:06 -0400

    Add code for Youtube Downloader GUI

--- Youtube-Downloader-GUI/Form1.Designer.cs
+++ Youtube-Downloader-GUI/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbEnterUrl = new System.Windows.Forms.TextBox();
+            this.lbEnterUrl = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lbDownloadStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbEnterUrl
+            // 
+            this.tbEnterUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEnterUrl.Location = new System.Drawing.Point(175, 319);
+            this.tbEnterUrl.Name = "tbEnterUrl";
+            this.tbEnterUrl.Size = new System.Drawing.Size(579, 38);
+            this.tbEnterUrl.TabIndex = 0;
+            // 
+            // lbEnterUrl
+            // 
+            this.lbEnterUrl.AutoSize = true;
+            this.lbEnterUrl.Location = new System.Drawing.Point(170, 276);
+            this.lbEnterUrl.Name = "lbEnterUrl";
+            this.lbEnterUrl.Size = new System.Drawing.Size(327, 25);
+            this.lbEnterUrl.TabIndex = 1;
+            this.lbEnterUrl.Text = "Enter a YouTube url to download";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDownload.Location = new System.Drawing.Point(816, 276);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(240, 81);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lbDownloadStatus
+            // 
+            this.lbDownloadStatus.AutoSize = true;
+            this.lbDownloadStatus.Location = new System.Drawing.Point(170, 402);
+            this.lbDownloadStatus.Name = "lbDownloadStatus";
+            this.lbDownloadStatus.Size = new System.Drawing.Size(0, 25);
+            this.lbDownloadStatus.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1255, 682);
+            this.Controls.Add(this.lbDownloadStatus);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.lbEnterUrl);
+            this.Controls.Add(this.tbEnterUrl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbEnterUrl;
+        private System.Windows.Forms.Label lbEnterUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lbDownloadStatus;
     }
 }
 
--- Youtube-Downloader-GUI/Form1.cs
+++ Youtube-Downloader-GUI/Form1.cs
@@ -1,20 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YoutubeDLSharp;
 
 namespace Youtube_Downloader_GUI
 {
     public partial class Form1 : Form
     {
+        public static readonly string DOWNLOAD_PATH = "C:\\transfer\\YouTube";
+        private YoutubeDL ytdlp;
+
         public Form1()
         {
+            ytdlp = new YoutubeDL();
             InitializeComponent();
+        }
+
+        // helper function for downloading the video from the url
+        private async Task DownloadFromUrl(string url)
+        {
+            // ensure that the output directory exists
+            if (!Directory.Exists(DOWNLOAD_PATH))
+            {
+                Directory.CreateDirectory(DOWNLOAD_PATH);
+            }
+
+            // point to the local executable file gotten from their documentation
+            ytdlp.YoutubeDLPath = "../../lib/yt-dlp.exe";
+
+            // set FFmpeg for combining audio with video
+            ytdlp.FFmpegPath = "../../lib/ffmpeg.exe"; 
+
+            // set output destination to the downloads folder and the output file name template
+            ytdlp.OutputFolder = DOWNLOAD_PATH;
+            ytdlp.OutputFileTemplate = "%(title)s.%(ext)s";
+
+            // attempt to download
+            var res = await ytdlp.RunVideoDownload(url);
+
+            if (!res.Success)
+            {
+                throw new Exception($"Download failed: {string.Join(Environment.NewLine, res.ErrorOutput)}");
+            }
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // disable the button to avoid more than one click event
+                btnDownload.Enabled = false;
+                lbDownloadStatus.Text = "Beginning download process...";
+
+                // get the url from the text box and ensure it is valid
+                string url = tbEnterUrl.Text;
+                if (string.IsNullOrEmpty(url))
+                {
+                    throw new Exception("Inputted url is null or empty");
+                }
+
+                // call the download from url function to try to download the video
+                lbDownloadStatus.Text = "Downloading...";
+                await DownloadFromUrl(url);
+
+                tbEnterUrl.Text = "";
+                lbDownloadStatus.Text = "Success! Find the downloaded video at C:\\transfer\\YouTube";
+            }
+            catch (Exception ex)
+            {
+                // display the error message
+                lbDownloadStatus.Text = $"Error: {ex.Message}";
+            } finally
+            {
+                // re enable the button at the end
+                btnDownload.Enabled = true;
+            }
         }
     }
 }
