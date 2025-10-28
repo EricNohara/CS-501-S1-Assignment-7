08a26dc542413e7222d4a0f0cd7e324f7e32f5f3
zaclan <lanyk@bu.edu>
2025-10-27 19:35:45 -0700

    Completed translator

--- Translator-GUI/Form1.Designer.cs
+++ Translator-GUI/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.outputLanguageComboBox = new System.Windows.Forms.ComboBox();
+            this.text_Textbox = new System.Windows.Forms.TextBox();
+            this.clear_Btn = new System.Windows.Forms.Button();
+            this.translate_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputLanguageComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLanguageComboBox
+            // 
+            this.outputLanguageComboBox.FormattingEnabled = true;
+            this.outputLanguageComboBox.Location = new System.Drawing.Point(371, 51);
+            this.outputLanguageComboBox.Name = "outputLanguageComboBox";
+            this.outputLanguageComboBox.Size = new System.Drawing.Size(121, 21);
+            this.outputLanguageComboBox.TabIndex = 0;
+            // 
+            // text_Textbox
+            // 
+            this.text_Textbox.Location = new System.Drawing.Point(211, 101);
+            this.text_Textbox.Multiline = true;
+            this.text_Textbox.Name = "text_Textbox";
+            this.text_Textbox.Size = new System.Drawing.Size(281, 97);
+            this.text_Textbox.TabIndex = 1;
+            // 
+            // clear_Btn
+            // 
+            this.clear_Btn.Location = new System.Drawing.Point(371, 223);
+            this.clear_Btn.Name = "clear_Btn";
+            this.clear_Btn.Size = new System.Drawing.Size(75, 23);
+            this.clear_Btn.TabIndex = 2;
+            this.clear_Btn.Text = "Clear";
+            this.clear_Btn.UseVisualStyleBackColor = true;
+            this.clear_Btn.Click += new System.EventHandler(this.clear_Btn_Click);
+            // 
+            // translate_Btn
+            // 
+            this.translate_Btn.Location = new System.Drawing.Point(252, 223);
+            this.translate_Btn.Name = "translate_Btn";
+            this.translate_Btn.Size = new System.Drawing.Size(75, 23);
+            this.translate_Btn.TabIndex = 3;
+            this.translate_Btn.Text = "Translate";
+            this.translate_Btn.UseVisualStyleBackColor = true;
+            this.translate_Btn.Click += new System.EventHandler(this.translate_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output Language";
+            // 
+            // inputLanguageComboBox
+            // 
+            this.inputLanguageComboBox.FormattingEnabled = true;
+            this.inputLanguageComboBox.Location = new System.Drawing.Point(211, 51);
+            this.inputLanguageComboBox.Name = "inputLanguageComboBox";
+            this.inputLanguageComboBox.Size = new System.Drawing.Size(121, 21);
+            this.inputLanguageComboBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input Language";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputLanguageComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.translate_Btn);
+            this.Controls.Add(this.clear_Btn);
+            this.Controls.Add(this.text_Textbox);
+            this.Controls.Add(this.outputLanguageComboBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox outputLanguageComboBox;
+        private System.Windows.Forms.TextBox text_Textbox;
+        private System.Windows.Forms.Button clear_Btn;
+        private System.Windows.Forms.Button translate_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox inputLanguageComboBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Translator-GUI/Form1.cs
+++ Translator-GUI/Form1.cs
@@ -5,16 +5,100 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GTranslate;
+using GTranslate.Translators;
 
 namespace Translator_GUI
 {
     public partial class Form1 : Form
     {
+        private readonly static GoogleTranslator _translator = new GoogleTranslator();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeLangugages();
+        }
+
+        private void InitializeLangugages()
+        {
+            var outputlanguageItems = Language.LanguageDictionary.Values
+               .Select(lang => new { Text = lang.Name, Value = lang.ISO6391 })
+               .ToList();
+
+            var inputlanguageItems = Language.LanguageDictionary.Values
+               .Select(lang => new { Text = lang.Name, Value = lang.ISO6391 })
+               .ToList();
+
+            outputLanguageComboBox.DataSource = outputlanguageItems;
+            outputLanguageComboBox.DisplayMember = "Text";
+            outputLanguageComboBox.ValueMember = "Value";
+
+            inputLanguageComboBox.DataSource = inputlanguageItems;
+            inputLanguageComboBox.DisplayMember = "Text";
+            inputLanguageComboBox.ValueMember = "Value";
+
+        }
+
+        private async Task TranslateText()
+        {
+            translate_Btn.Enabled = false;
+            translate_Btn.Text = "Translating...";
+
+            try
+            {
+                var inputLanguage = (string)inputLanguageComboBox.SelectedValue;
+                var outputLangauge = (string)outputLanguageComboBox.SelectedValue;
+                var inputText = text_Textbox.Text;
+
+                var result = await _translator.TranslateAsync(inputText, outputLangauge, inputLanguage);
+                MessageBox.Show($"Source input: {result.Source}. Translated: {result.Translation}");
+
+                text_Textbox.Text = result.Translation;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"An error occurred during translation. Please try again. {e}",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                translate_Btn.Enabled = true;
+                translate_Btn.Text = "Translate";
+            }
+        }
+
+        private async void translate_Btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(text_Textbox.Text))
+            {
+                MessageBox.Show("Please enter text to translate.", "Warning",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                await TranslateText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Translation failed: {ex.Message}", "Error",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void clear_Btn_Click(object sender, EventArgs e)
+        {
+            text_Textbox.Clear();
+        }
     }
-}
+    }
